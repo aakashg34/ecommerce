0e242330e2b6344280216bbeff717d8b8f157a84
shubhambaranwal158 <shubham.baranwal@pal.tech>
2024-06-24 16:59:59 +0530

    login page

--- ecommerce-website/src/app/login/login.component.ts
+++ ecommerce-website/src/app/login/login.component.ts
@@ -1,15 +1,38 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-
+// import { AuthService } from '../auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-login',
   standalone: true,
-  imports: [],
+  selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css'],
+  // providers: [AuthService],  // Ensure HttpClient and AuthService are provided
+  imports: [CommonModule, FormsModule]
 })
 export class LoginComponent {
-  
+  username: string = '';
+  password: string = '';
+
+  constructor(private router: Router, 
+    // private authService: AuthService
+  ) {}
+
+  login() {
+    
+    // this.authService.login(this.username, this.password).subscribe(
+    //   response => {
+    //     if (response) {
+    //       this.router.navigate(['/catalog']);
+    //     } else {
+    //       alert('Invalid credentials');
+    //     }
+    //   },
+    //   error => {
+    //     alert('Login failed');
+    //   }
+    // );
+  }
 }
